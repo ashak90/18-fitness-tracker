251ac971498f4891dacba11797e30957f323997c
Asha Kumar <ashakumar90@gmail.com>
2021-07-14 13:51:24 -0400

    added to the api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,8 +14,17 @@ module.exports = function (app) {
                     message: "Failed to load workouts"
                 });
             });
-
     });
 
-    
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body)
+            .then((addWorkout) => {
+                res.json(addWorkout);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    });
+
+
 }
